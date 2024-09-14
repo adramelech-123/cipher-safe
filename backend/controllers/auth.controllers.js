ae24c26edb6264c5cf07d86fa8d0770e7c86bbb9
adramelech-123 <tmamvuto6@gmail.com>
2024-09-14 11:58:03 +0200

    Login, logout, forgot-password and reset password routes - complete

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,8 +1,9 @@
 import { User } from "../models/user.model.js";
 import bcrypt from 'bcryptjs'
+import crypto from "crypto"
 import { generateVerificationCode } from "../utils/generateVerificationCode.js";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/emails.js";
+import { sendPasswordResetEmail, sendResetSuccessEmail, sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/emails.js";
 
 
 export const signup = async (req, res) => {
@@ -88,3 +89,127 @@ export const verifyEmail = async(req, res) => {
     res.status(500).json({ success: false, message: "Server error" });
   }
 }
+
+export const login = async(req, res) => {
+  const {email, password} = req.body
+
+  try {
+    const user = User.findOne({email})
+
+    if(!user) {
+      return res.status(400).json({success: false, message: "This user does not exist!"})
+    }
+
+    const passwordIsValid = await bcrypt.compare(password, user.password)
+    if(!passwordIsValid) {
+      return res.status(400).json({success: false, message: "Invalid credentials"})
+    }
+
+    generateTokenAndSetCookie(res, user._id)
+
+    user.lastLogin = new Date()
+    await user.save()
+
+    res.status(200).json({
+      success: true,
+      message: "User logged in successfully! ✅",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+}
+
+export const logout = async (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({ success: true, message: "Logged out succesfully." });
+};
+
+export const forgotPassword = async (req, res) => {
+  const {email} = req.body
+
+  try {
+    const user = User.findOne({email})
+
+    if(!user) {
+      return res.status(400).json({success: false, message: "User not found!"})
+    }
+
+    // Generate Reset Token and token expiry date
+    const resetToken = crypto.randomBytes(20).toString("hex")
+    const resetTokenExpiresAt = Date.now() + 1 * 60 * 60 * 1000
+
+    // Set User password reset token and expiry
+    user.resetPasswordToken = resetToken
+    user.resetPasswordTokenExpiresAt = resetTokenExpiresAt
+
+    await user.save()
+
+    // Send Password Reset Email
+    sendPasswordResetEmail(user.email, `${process.env.CLIENT_URL}/reset-password/${resetToken}`)
+    res
+      .status(200)
+      .json({ success: true, message: "Password reset link sent to email" });
+
+  } catch (error) {
+    console.log("Error in forgot password", error);
+    res.status(400).json({ success: false, message: error.message });
+  }
+}
+
+export const resetPassword = async (req, res) => {
+  const {token} = req.params
+  const {password} = req.body
+
+  try {
+    const user = User.findOne({
+      resetPasswordToken: token,
+      resetPasswordTokenExpiresAt: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid or expired reset token" });
+    }
+
+    // Update Password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user.password = hashedPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordTokenExpiresAt = undefined;
+    await user.save();
+
+    await sendResetSuccessEmail(user.email);
+    res
+      .status(200)
+      .json({ success: true, message: "Password reset successful" });
+
+  } catch (error) {
+    console.log("Error in resetPassword", error);
+    res.status(400).json({ success: false, message: error.message });
+  }
+
+}
+
+export const checkAuth = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select("-password");
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found!" });
+    }
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log("Error in checkAuth", error);
+
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,23 +1,25 @@
 import express from 'express'
 import { checkSchema, validationResult } from "express-validator";
 import { userValidationSchema } from '../utils/validationSchemas.js';
-import { signup, verifyEmail } from '../controllers/auth.controllers.js';
+import { checkAuth, forgotPassword, login, logout, resetPassword, signup, verifyEmail } from '../controllers/auth.controllers.js';
 
 const router = express.Router()
 
+router.get('/check-auth', checkAuth)
 router.post('/signup', checkSchema(userValidationSchema), (req, res, next) => {
     const validationErrors = validationResult(req);
 
     if (!validationErrors.isEmpty()) {
     //   const errorMessage = validationErrors.array().map((error) => error.msg);
-    
       return res.status(400).json({ errors: validationErrors.array() });
     }
-
     next();
-
 }, signup)
 
 router.post('/verify-email', verifyEmail)
+router.post('/login', login)
+router.post('/logout', logout)
+router.post('/forgot-password', forgotPassword)
+router.post('/reset-password/:token', resetPassword)
 
 export default router
