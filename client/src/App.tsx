809fe38c16b87b95b4958f0d4d8cbcb8f90f49ac
adramelech-123 <tmamvuto6@gmail.com>
2024-09-14 23:00:02 +0200

    Shadcn test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,21 @@
-
+import { Button } from "./components/ui/button"
 
 function App() {
 
-
   return (
     <>
-      <h1>Hello</h1>
+      <div className="flex justify-center p-6 w-full ">
+        <Button
+          variant="default"
+          size={"lg"}
+          className="rounded-full"
+          onClick={() => alert("Clicked")}
+        >
+          Click me
+        </Button>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
